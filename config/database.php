496f5e35cab04ee538c5a6afed9a032125c10e71
Guilherme Tomé <guigo.reis90@gmail.com>
2022-10-05 20:39:36 -0300

    Configured read replicas support in mysql database

--- config/database.php
+++ config/database.php
@@ -44,9 +44,14 @@ return [
         ],
 
         'mysql' => [
+            'read' => [
+                'host' => env('DB_READ_HOST', '127.0.0.1'),
+            ],
+            'write' => [
+                'host' => env('DB_WRITE_HOST', '127.0.0.1')
+            ],
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -125,7 +130,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
